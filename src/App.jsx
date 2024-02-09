4ca4a8b69823d9e273ab4d12c5258b40deb728f2
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-02-09 20:19:09 +0530

    Lading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Landing from "./components/Landing/Landing";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 text-white">
       <Navbar />
+      <Landing/>
     </div>
   );
 };
