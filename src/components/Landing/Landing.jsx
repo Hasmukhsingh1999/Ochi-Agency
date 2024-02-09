ae0142db12f5ff712420e6371c158e7a879aa698
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-02-09 20:33:35 +0530

    feat: lanfing page optimise

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,21 +2,13 @@ const Landing = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 pt-1">
       <div className="textstructure mt-40 px-20">
-        <div className="masker font-Neueue-Montreal">
-          <h1 className="uppercase text-9xl  -leading-[-4vw] tracking-tighter font-md">
-            We Create
-          </h1>
-        </div>
-        <div className="masker font-Neueue-Montreal">
-          <h1 className="uppercase text-9xl  -leading-[-4vw] tracking-tighter font-md">
-            Eye-Opening
-          </h1>
-        </div>
-        <div className="masker font-Neueue-Montreal">
-          <h1 className="uppercase text-9xl  -leading-[-4vw] tracking-tighter font-md">
-            Presentations
-          </h1>
-        </div>
+        {["We Cretae", "Eye Opening", "Presentations"].map((item, index) => (
+          <div className="masker font-Neueue-Montreal" key={index}>
+            <h1 className="uppercase text-9xl  -leading-[-4vw] tracking-tighter font-md">
+              {item}
+            </h1>
+          </div>
+        ))}
       </div>
     </div>
   );
