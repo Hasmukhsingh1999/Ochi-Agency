2d60193c96a6b1b2ee82841ea4f7d3509a6fb3bd
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-02-10 14:23:24 +0530

    final commit

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,4 +1,14 @@
+import { motion, useAnimation } from "framer-motion";
+
 const Features = () => {
+  const cards = [useAnimation(), useAnimation()];
+  const handleHover = (index) => {
+    cards[index].start({ y: "0" });
+  };
+  const handleHoverEnd = (index) => {
+    console.log("Hover end triggered for index:", index);
+    cards[index].start({ y: "100%" });
+  };
   return (
     <div className="w-full py-20">
       <div className="w-full px-20 border-b-[2px] border-zinc-700 pb-20">
@@ -6,10 +16,25 @@ const Features = () => {
       </div>
       <div className="px-20">
         <div className="cards w-full flex gap-10 mt-10">
-          <div className="card w-1/2 h-[75vh] cardcontainer relative">
-            <h1 className="absolute -translate-x-24 text-[#CDEA68] z-[9] text-8xl -pb-20 leading-none tracking-tighter left-full top-1/2 -translate-y-1/2">
-            {"FYDE".split("").map((item, index) => (
-                <span key={index} className="">{item}</span>
+          <motion.div
+            onHoverStart={() => handleHover(0)}
+            onHoverEnd={() => handleHoverEnd(0)}
+            className="card w-1/2 h-[75vh] cardcontainer relative"
+          >
+            <h1 className="absolute -translate-x-24 text-[#CDEA68] z-[9] text-8xl -pb-20 leading-none tracking-tighter left-full top-1/2 -translate-y-1/2 flex overflow-hidden">
+              {"FYDE".split("").map((item, index) => (
+                <motion.span
+                  key={index}
+                  className="inline-block"
+                  initial={{ y: "100%" }}
+                  animate={cards[0]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    duration: index * 0.6,
+                  }}
+                >
+                  {item}
+                </motion.span>
               ))}
             </h1>
             <div className="w-full h-full  rounded-lg overflow-hidden">
@@ -18,11 +43,26 @@ const Features = () => {
                 className="w-full h-full object-cover"
               />
             </div>
-          </div>
-          <div className="card w-1/2 h-[75vh] cardcontainer relative">
-            <h1 className="absolute translate-x-1/2 text-[#CDEA68] z-[9] text-8xl -pb-20 leading-none tracking-tighter right-full top-1/2 -translate-y-1/2">
+          </motion.div>
+          <motion.div
+            className="card w-1/2 h-[75vh] cardcontainer relative"
+            onHoverStart={() => handleHover(1)}
+            onHoverEnd={() => handleHoverEnd(1)}
+          >
+            <h1 className="absolute translate-x-1/2 text-[#CDEA68] z-[9] text-8xl -pb-20 leading-none tracking-tighter right-full top-1/2 -translate-y-1/2 flex overflow-hidden">
               {"VISE".split("").map((item, index) => (
-                <span key={index} className="">{item}</span>
+                <motion.span
+                  key={index}
+                  className="inline-block"
+                  initial={{ y: "100%" }}
+                  animate={cards[1]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    duration: index * 0.6,
+                  }}
+                >
+                  {item}
+                </motion.span>
               ))}
             </h1>
             <div className="w-full h-full  rounded-lg overflow-hidden">
@@ -31,7 +71,7 @@ const Features = () => {
                 className="w-full h-full object-cover"
               />
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
